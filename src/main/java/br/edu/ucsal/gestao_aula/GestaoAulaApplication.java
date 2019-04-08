a5ae26b54f7775b70c105d2c6399ad9b657dc166
GIOVANNI DE LIMA CONCEICAO <100942064@ucsal.br>
2019-04-08 09:18:15 -0300

    Mapeamento das classes no projeto

---
+++ src/main/java/br/edu/ucsal/gestao_aula/GestaoAulaApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ucsal.gestao_aula;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoAulaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoAulaApplication.class, args);
+	}
+
+}
